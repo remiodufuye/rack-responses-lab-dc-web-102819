4bc739b80a25f66c0bb99027d90cd87013033c40
remiodufuye <remi.odufuye@gmail.com>
2019-11-16 15:01:19 -0500

    initial commit

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,29 @@
 class Application
-
+ 
+    @@items = ["Apples","Carrots","Pears"]
+   
     def call(env)
       resp = Rack::Response.new
-      now = Time.now
-      if now.hour >=12
-        resp.write "Good Afternoon"
+      req = Rack::Request.new(env)
+   
+      if req.path.match(/items/)
+        @@items.each do |item|
+          resp.write "#{item}\n"
+        end
+      elsif req.path.match(/search/)
+   
+        search_term = req.params["q"]
+   
+        if @@items.include?(search_term)
+          resp.write "#{search_term} is one of our items"
+        else
+          resp.write "Couldn't find #{search_term}"
+        end
+   
       else
-        resp.write "Good Morning"
+        resp.write "Path Not Found"
       end
+   
       resp.finish
     end
-  
   end
